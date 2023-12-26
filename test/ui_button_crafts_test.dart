0f05eae442b5e81a3677593984a114d052d59b70
Ritu <ritu.n@applocum.org>
2023-12-26 17:32:49 +0530

    Added HomePage url in pubspec

--- test/ui_button_crafts_test.dart
+++ test/ui_button_crafts_test.dart
@@ -1,12 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ui_button_crafts/ui_button_crafts.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    UIButtonCrafts(
+      onPressed: () {
+        // Do something when the button is pressed
+      },
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      backgroundColor: Colors.green,
+      text: 'Custom Button',
+      textStyle: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+        // Add any other text style properties as needed
+      ),
+    );
   });
 }
